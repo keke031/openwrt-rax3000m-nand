8bd5d7a094b0b698625e16566d471a5d83be2617
keke031 <26167020@qq.com>
2024-07-27 02:18:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -34,6 +34,7 @@ sed -i '$a src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf
 # git clone https://github.com/jerrykuku/lua-maxminddb     #git lua-maxminddb 依赖
 # git clone https://github.com/jerrykuku/luci-app-vssr
 # git clone https://github.com/xiaorouji/openwrt-passwall2  #passwall出国软件
+git clone https://github.com/Siriling/5G-Modem-Support
 
 # rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
 # 全新的[argon-主题]此主题玩法很多,这里看说明【https://github.com/jerrykuku/luci-theme-argon/blob/18.06/README_ZH.md】
